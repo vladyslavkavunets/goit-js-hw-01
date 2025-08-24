5f84cc341eab5a1c8321722cb7f41f1590832c42
Vladyslav Kavunets <vladikkavunets@gmail.com>
2025-08-24 15:42:25 +0200

    Submitting homework

--- js/task-3.js
+++ js/task-3.js
@@ -2,9 +2,9 @@ function getElementWidth(content, padding, border) {
   const contentWidth = parseFloat(content);
   const paddingWidth = parseFloat(padding);
   const borderWidth = parseFloat(border);
-  
-  const totalWidth = contentWidth + (paddingWidth * 2) + (borderWidth * 2);
-  
+
+  const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
+
   return totalWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
